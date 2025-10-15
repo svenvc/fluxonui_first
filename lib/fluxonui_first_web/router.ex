21f8e684bac9e170ffdc3fcb829103c2884b0d56
Sven Van Caekenberghe <sven@stfx.eu>
2025-10-15 11:59:41 +0200

    fix mistake in router.ex

--- lib/fluxonui_first_web/router.ex
+++ lib/fluxonui_first_web/router.ex
@@ -50,6 +50,11 @@ defmodule FluxonUIFirstWeb.Router do
 
     live_session :require_authenticated_user,
       on_mount: [{FluxonUIFirstWeb.UserAuth, :require_authenticated}] do
+      live "/", TodoLive.Index, :index
+      live "/todos", TodoLive.Index, :index
+      live "/todos/new", TodoLive.Form, :new
+      live "/todos/:id", TodoLive.Show, :show
+      live "/todos/:id/edit", TodoLive.Form, :edit
       live "/users/settings", UserLive.Settings, :edit
       live "/users/settings/confirm-email/:token", UserLive.Settings, :confirm_email
     end
@@ -62,11 +67,6 @@ defmodule FluxonUIFirstWeb.Router do
 
     live_session :current_user,
       on_mount: [{FluxonUIFirstWeb.UserAuth, :mount_current_scope}] do
-      live "/", TodoLive.Index, :index
-      live "/todos", TodoLive.Index, :index
-      live "/todos/new", TodoLive.Form, :new
-      live "/todos/:id", TodoLive.Show, :show
-      live "/todos/:id/edit", TodoLive.Form, :edit
       live "/users/register", UserLive.Registration, :new
       live "/users/log-in", UserLive.Login, :new
       live "/users/log-in/:token", UserLive.Confirmation, :new
