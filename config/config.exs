bf06c6a6ee12206ccbcc0d5b6aad08dd4c784c72
Sven Van Caekenberghe <sven@stfx.eu>
2025-10-15 10:26:51 +0200

    mix phx.gen.auth Accounts User users --live

--- config/config.exs
+++ config/config.exs
@@ -7,6 +7,19 @@
 # General application configuration
 import Config
 
+config :fluxonui_first, :scopes,
+  user: [
+    default: true,
+    module: FluxonUIFirst.Accounts.Scope,
+    assign_key: :current_scope,
+    access_path: [:user, :id],
+    schema_key: :user_id,
+    schema_type: :id,
+    schema_table: :users,
+    test_data_fixture: FluxonUIFirst.AccountsFixtures,
+    test_setup_helper: :register_and_log_in_user
+  ]
+
 config :fluxonui_first,
   namespace: FluxonUIFirst,
   ecto_repos: [FluxonUIFirst.Repo],
